af202a3df2144b27dad7e183948d36a5e3652510
Alexander Anich <alexander.anich@gmail.com>
2015-09-09 11:45:39 -1000

    Create Engine class extends from Steel

---
+++ Engine.js
@@ -0,0 +1,12 @@
+import Steel from './Steel';
+
+export default class Engine extends Steel {
+
+  constructor (name, cylinders, fuelType, horsepower) {
+    super(density);
+    this._name = name;
+    this._cylinders = cylinders;
+    this._fuelType = fuelType;
+    this._horsepower = horsepower;
+  }
+}
