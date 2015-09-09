21420fe5b452ad974222112acb7871d63b26ba43
Alexander Anich <alexander.anich@gmail.com>
2015-09-09 11:48:45 -1000

    Create Cadillac class extends Car

---
+++ Cadillac.js
@@ -0,0 +1,10 @@
+import Car from './Car';
+
+export default class Cadillac extends Car{
+
+  constructor (name, year, model) {
+    super(name, weight);
+    this._year = year;
+    this._model = model;
+  }
+}
