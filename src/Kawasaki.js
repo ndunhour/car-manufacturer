32f52baeddd3039a2a4780c5b22664e9c48860ed
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-09 21:39:22 -1000

    add getter and setter

--- src/Kawasaki.js
+++ src/Kawasaki.js
@@ -7,4 +7,18 @@ export default class Kawasaki extends Motorcycle{
     this._year = year;
     this._model = model;
   }
+
+  get year() {
+    return this._year;
+  }
+  set year(y) {
+    this._year = y;
+  }
+
+  get model() {
+    return this._model;
+  }
+  set model(m) {
+    this._model = m;
+  }
 }
