a4184a6d1686e53044ce034154891bf3dc4f4af6
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-09 21:34:51 -1000

    add getter and setter

--- src/Engine.js
+++ src/Engine.js
@@ -9,4 +9,34 @@ export default class Engine extends Steel {
     this._fuelType = fuelType;
     this._horsepower = horsepower;
   }
+  get name() {
+    return this._name;
+  }
+  set name(n) {
+    this._name = n;
+  }
+
+  get cylinders() {
+    return this._cylinders;
+  }
+  set cylinders(c)  {
+    this._cylinders = c;
+  }
+
+  get fuelType() {
+    return this._fuelType;
+  }
+  set fuelType(fT) {
+    this._fuelType = fT;
+  }
+
+  get horsepower() {
+    return this._horsepower;
+  }
+  set horsepower(h) {
+    this._horsepower = h;
+  }
+
+
+
 }
