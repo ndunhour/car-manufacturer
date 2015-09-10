208b0424d26d5ef021fdbb6a5d2418962db181cc
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-09 21:44:09 -1000

    add getter and setter

--- src/Steel.js
+++ src/Steel.js
@@ -3,4 +3,10 @@ export default class Steel {
   constructor (density) {
     this._density = density;
   }
+  get density() {
+    return this._density;
+  }
+  set density(d) {
+    this._density = d;
+  }
 }
