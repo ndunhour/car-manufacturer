891458f366a517c26d4aec447af2bfb11f9597b4
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-09 21:27:08 -1000

    add getter and setter

--- src/Car.js
+++ src/Car.js
@@ -6,4 +6,11 @@ export default class Car extends Vehicle{
     super(name, cylinders, fuelType, horsepower, body, true);
     this._weight = weight;
   }
+
+  get weight() {
+    return this._weight;
+  }
+  set weight(w) {
+    this._weight = w;
+  }
 }
