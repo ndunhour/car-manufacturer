ee263c6fba73c41384e9fb1e1f8e02e5376edb96
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-09 21:46:38 -1000

    add getter and setter

--- src/Truck.js
+++ src/Truck.js
@@ -7,4 +7,18 @@ export default class Truck extends Vehicle{
     this._weight = weight;
     this._lifted = lifted;
   }
+
+  get weight() {
+    return this._weight;
+  }
+  set weight(w) {
+    this._weight = w;
+  }
+
+  get lifted() {
+    return this._lifted;
+  }
+  set lifted(l) {
+    this._lifted = l;
+  }
 }
